9a596bf463e45b93c889ae3821cbc42c56ffa108
Atai Chynaliev <chynaliev.ae@gmail.com>
2016-03-19 14:03:45 -0700

    if, elif, else

---
+++ python/ex29.py
@@ -0,0 +1,29 @@
+people  = 20
+cats = 30
+dogs = 15
+
+
+if people < cats:
+	print "Too many cats! The World is doomed!"
+
+if people > cats:
+	print "Not many cats! The World is saved!"
+
+if people < dogs:
+	print "The World is drooled on!"
+
+if people > dogs:
+	print "The World is dry!"
+
+
+dogs += 5
+
+if people >= dogs:
+	print "People are greater then or equal to dogs."
+
+if people <= dogs:
+	print "People are less then or equal to dogs."
+
+if people == dogs:
+	print "Peopel are dogs."
+	
